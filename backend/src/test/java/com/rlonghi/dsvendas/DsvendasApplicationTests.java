97030af3dd43a4cf7708e3dd32a51425ec45190b
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-05 00:15:31 -0300

    Project created

---
+++ backend/src/test/java/com/rlonghi/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlonghi.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
