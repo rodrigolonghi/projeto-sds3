78dcea69b05c78159cdc5ff7e6238e831493dd59
Rodrigo Longhi <rfslonghi@gmail.com>
2021-05-05 22:10:20 -0300

    Layers

---
+++ backend/src/main/java/com/rlonghi/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,10 @@
+package com.rlonghi.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rlonghi.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+	
+}
